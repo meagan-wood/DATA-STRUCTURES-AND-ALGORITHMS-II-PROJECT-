d5abde4539c3bf6114fdaaf0e478502c43934536
Meagan Wood <wmeagan508@gmail.com>
2023-02-02 15:32:31 -0700

    2nd commit

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from hashTable import myHash, read_package_data
+from truck import distance_data, address_data, distance_2dlist
 
 for i in range(len(myHash.table) + 1):
     print("Key: {} and Package: {}".format(i + 1, myHash.search(i + 1)))
-
 
 print("\nSearch:")
 print(myHash.search(22))
 print(myHash.search(37))
 
+print("\nDistance:")
+# print(distance_data[3][2])
+print(distance_2dlist[1][1])
+print("\nAddress:")
+print(address_data)
--- truck.py
+++ truck.py
@@ -1,3 +1,8 @@
+import csv
+
+import numpy as numpy
+
+
 class Truck:
 
     def __init__(self, capacity, speed, load, packages):
@@ -11,3 +16,27 @@ class Truck:
     def __str__(self):
         return "%s, %s, %s, %s" % (self.capacity, self.speed, self.load, self.packages)
 
+
+# distance_data = list(csv.reader(open('DistanceTable.csv'), delimiter=','))
+distance_data = open("DistanceTable.csv")
+distance_2dlist = numpy.genfromtxt(distance_data, delimiter=",")
+
+address_data = list(csv.reader(open('AddressFile.csv'), delimiter=','))
+
+
+def lookup_address():
+    return address_data
+
+
+def get_mileage_total(row, column, mileage_total):
+    mileage = distance_2dlist[row][column]
+    if mileage == '':
+        mileage = distance_2dlist[column][row]
+    return mileage_total + float(mileage)
+
+
+def get_mileage(row, column):
+    mileage = distance_2dlist[row][column]
+    if mileage == '':
+        mileage = distance_2dlist[column][row]
+    return float(mileage)
